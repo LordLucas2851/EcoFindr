fb98cc7eb33af3790efc0c63fc51c4127b011940
LordLucas2851 <lucas.shaver@lander.edu>
2024-11-22 23:29:35 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,16 @@
-// Firebase initialization
+// Firebase configuration object (from your provided config)
 const firebaseConfig = {
-    apiKey: "AIzaSyBq9shDTSKIE7lfO8ydnuThm_JxOHMBSfc",
-    authDomain: "ecofindr-be091.firebaseapp.com",
-    projectId: "ecofindr-be091",
-    storageBucket: "ecofindr-be091.firebasestorage.app",
-    messagingSenderId: "960864865608",
-    appId: "1:960864865608:web:4994220d1cdf9c6d2b777d",
-    measurementId: "G-577X5TWXTT"
+  apiKey: "AIzaSyBq9shDTSKIE7lfO8ydnuThm_JxOHMBSfc",
+  authDomain: "ecofindr-be091.firebaseapp.com",
+  projectId: "ecofindr-be091",
+  storageBucket: "ecofindr-be091.firebasestorage.app",
+  messagingSenderId: "960864865608",
+  appId: "1:960864865608:web:4994220d1cdf9c6d2b777d",
+  measurementId: "G-577X5TWXTT"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Optionally, you can enable Firebase analytics if you'd like to track user activity (e.g. for the `measurementId`):
+// firebase.analytics();
