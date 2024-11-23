0968e17165535c12ee9f0c15d9d5489dcaa29946
LordLucas2851 <lucas.shaver@lander.edu>
2024-11-22 21:43:54 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,20 @@
+// Import the necessary Firebase functions
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 
+// Your Firebase project configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBq9shDTSKIE7lfO8ydnuThm_JxOHMBSfc",
     authDomain: "ecofindr-be091.firebaseapp.com",
     projectId: "ecofindr-be091",
-    storageBucket: "ecofindr-be091.appspot.com",
+    storageBucket: "ecofindr-be091.firebasestorage.app",
     messagingSenderId: "960864865608",
     appId: "1:960864865608:web:4994220d1cdf9c6d2b777d",
     measurementId: "G-577X5TWXTT"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Auth
+const auth = getAuth(app);
