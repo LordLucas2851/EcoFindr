f40e616e149f07b45bb2eaf55100e6ed5c4ace8f
LordLucas2851 <lucas.shaver@lander.edu>
2024-11-24 18:33:32 -0500

    Create Login-System.js

---
+++ Login-System.js
@@ -0,0 +1,7 @@
+document.getElementById("login-btn").addEventListener("click", () => {
+    alert("Login functionality is not yet implemented.");
+});
+
+document.getElementById("sign-up-btn").addEventListener("click", () => {
+    alert("Sign-up functionality is not yet implemented.");
+});
