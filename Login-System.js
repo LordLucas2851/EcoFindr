2dbd0b6a419cf64d0364009dd78812f07de59d2e
LordLucas2851 <lucas.shaver@lander.edu>
2024-11-24 20:08:16 -0500

    Delete Login-System.js

--- Login-System.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("login-btn").addEventListener("click", () => {
-    alert("Login functionality is not yet implemented.");
-});
-
-document.getElementById("sign-up-btn").addEventListener("click", () => {
-    alert("Sign-up functionality is not yet implemented.");
-});
