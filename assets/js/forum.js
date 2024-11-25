87eb9f89e5fc550a84b8c214811b0ece88917a4e
LordLucas2851 <lucas.shaver@lander.edu>
2024-11-25 16:00:06 +0000

    Update forum.js

--- assets/js/forum.js
+++ assets/js/forum.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, query, orderBy } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+import { getFirestore, collection, addDoc, query, getDocs, orderBy } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
 // Firebase configuration
 const firebaseConfig = {
