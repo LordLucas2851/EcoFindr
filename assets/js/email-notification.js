d6f1efb5f4e22ceb088b8c7248100361025ce610
LordLucas2851 <lucas.shaver@lander.edu>
2024-11-26 13:54:40 -0500

    Update email-notification.js

--- assets/js/email-notification.js
+++ assets/js/email-notification.js
@@ -1,20 +1,15 @@
-import emailjs from 'https://cdn.jsdelivr.net/npm/emailjs-com@2.6.4/dist/email.min.js';
+import emailjs from 'emailjs-com';
 
-//Function to send an email
 export const sendEmail = async (toEmail, fromName, message) => {
     try {
-        if (!toEmail || !fromName || !message) {
-            throw new Error('All fields are required.');
-        }
-
         const templateParams = {
             to_name: toEmail,
             from_name: fromName,
             message: message,
         };
 
-        await emailjs.send("service_8bwgvkk", "template_fy5gkdz", templateParams, "euggqJf1pklcQY6Uo");
-        console.log('Email sent successfully');
+        const response = await emailjs.send("service_8bwgvkk", "template_fy5gkdz", templateParams, "euggqJf1pklcQY6Uo");
+        console.log('Email sent successfully:', response);
     } catch (error) {
         console.error('Error sending email:', error);
     }
